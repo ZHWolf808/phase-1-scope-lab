b2e7ae217ce2eb440ee2ab103383faa427285208
Zachary Wolf <wolfislnd8@gmail.com>
2023-04-11 15:38:36 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//
+ var bestCustomer;
+function setBestCustomer(){
+return bestCustomer = 'not bob';
+}
+//
+ 
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+//
+const leastFavoriteCustomer = 'wolf';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'reuben';
+
+}
